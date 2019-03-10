f8d351ab70999c1226d9effae9c813ee5c9acc91
yanyan155 <jm@tut.by>
2019-03-10 16:08:47 +0300

    add npm & node version, port correct

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use(function(err, req, res, next) {
   if (!err.statusCode) err.statusCode = 500;
   res.status(err.statusCode).send(err.message); 
 });
-const server = app.listen(8080, function () {
+let port = process.env.PORT || 8080;
+const server = app.listen(port, "0.0.0.0", function () {
    const host = server.address().address;
    const port = server.address().port;
    console.log(`Example app listening at http://127.0.0.1:8080/`);
